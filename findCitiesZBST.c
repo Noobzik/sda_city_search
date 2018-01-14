b3b2c7b72f07909ad56c42b37d1bb0d899640299
Unknown <raki_b@live.fr>
2018-01-14 09:00:21 +0100

    Space optimization for Morton

--- findCitiesZBST.c
+++ findCitiesZBST.c
@@ -6,7 +6,7 @@
 /*   By: NoobZik <rakib.hernandez@gmail.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/17 19:46:00 by NoobZik           #+#    #+#             */
-/*   Updated: 2017/12/29 01:08:24 by NoobZik          ###   ########.fr       */
+/*   Updated: 2018/01/14 08:58:55 by NoobZik          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,10 +21,6 @@
 int comparison_fn_t (const void *a, const void *b);
 int comparison_double_t(double a, double b);
 
-typedef struct u_unionCity {
-  const City  *city;
-  uint64_t    coded;
-}             s_unionCity_t;
 /* ------------------------------------------------------------------------- *
  * Find all the cities in a box.
  *
@@ -50,7 +46,6 @@ LinkedList* findCities(LinkedList* cities,
 
   const City          *city;
   LinkedList          *filtered;
-  s_unionCity_t       *unionCity;
   BinarySearchTree    *bst     = newBST(&comparison_fn_t);
   LinkedList          *res     = newLinkedList();
   LLNode              *curr    = cities->head;
@@ -59,9 +54,10 @@ LinkedList* findCities(LinkedList* cities,
   uint64_t            max      = zEncode(latitudeMax, longitudeMax);
   uint64_t            *ptr_min = &min;
   uint64_t            *ptr_max = &max;
+  uint64_t            *codedMorton;
   int                 i        = 0;
 
-  unionCity = malloc(sizeof(s_unionCity_t)* cities->size);
+  codedMorton = malloc(sizeof(uint64_t)* cities->size);
   city = (const City*)curr->value;
 
   assert(-90 <= latitudeMin && latitudeMax <= 90);
@@ -69,9 +65,9 @@ LinkedList* findCities(LinkedList* cities,
 
   while (!error && curr != NULL) {
     assert(i < (int) cities->size);
-    unionCity[i].city = city = (const City *) curr->value;
-    unionCity[i].coded = zEncode(city->latitude, city->longitude);
-    error = error || !insertInBST(bst, &unionCity[i].coded, unionCity[i].city);
+    city = (const City *) curr->value;
+    codedMorton[i] = zEncode(city->latitude, city->longitude);
+    error = error || !insertInBST(bst, &codedMorton[i], city);
     curr = curr->next;
     i++;
   }
@@ -111,7 +107,7 @@ LinkedList* findCities(LinkedList* cities,
   }
 
   freeBST(bst, false);
-  free(unionCity);
+  free(codedMorton);
   freeLinkedList(filtered, false);
   return res;
 }
@@ -134,8 +130,8 @@ int comparison_fn_t(const void* a, const void* b) {
 }
 
 /**
- * Compare two Unit64 types
- *
+ * Compare two double types
+ * To be used only by findCitiesZBST.c
  * a < b    <=>   foo(a,b) < 0
  * a > b    <=>   foo(a,b) > 0
  * a = b    <=>   foo(a,b) == 0
